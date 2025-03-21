05c07b3763575032645b932c62d1a3b540976d98
fatimahanif1045 <fatimahanif1045@gmail.com>
2025-03-21 15:53:02 +0500

    .

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import TodoList from './components/TodoList';
 function App() {
   const [tasks, setTasks] = useState([
     { id: 1, text: 'Task 1' },
-    { id: 2, text: 'Task 2' },
+    { id: 2, text: 'Task 2 do assignment' },
     { id: 3, text: 'Task 3' },
   ]);
 
   const handleDelete = (taskId) => {
     const updatedTasks = tasks.filter(task => task.id !== taskId);
-    setTasks(updatedTasks); // Update the state with the filtered tasks
+    setTasks(updatedTasks); 
   };
   
   return (
